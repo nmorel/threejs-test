50cc30175202bdf1b70594db1570a0506b046cd9
Nicolas Morel <nicolas.morel@klaxoon.com>
2022-01-30 11:14:06 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './index.css'
+import {PerspectiveCamera, Scene, WebGLRenderer} from 'three'
+
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
