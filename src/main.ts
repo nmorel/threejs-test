6a53c05d2e400b39d5d01db72e496d9d4a3cf939
Nicolas Morel <nicolas.morel@klaxoon.com>
2022-01-30 14:34:13 +0100

    use InstancedMesh to improve performance

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,33 @@
 import "./index.css";
 import {
   BoxGeometry,
+  Color,
   CubeTextureLoader,
+  InstancedBufferGeometry,
+  InstancedMesh,
+  Matrix4,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
   PlaneGeometry,
+  Quaternion,
   Scene,
+  ShaderMaterial,
+  Vector3,
   WebGLRenderer,
 } from "three";
-import Stats from 'three/examples/jsm/libs/stats.module'
+import Stats from "three/examples/jsm/libs/stats.module";
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
+// @ts-ignore
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.00001,
-  100000
+  100_000
 );
 camera.position.z = 1000;
 
@@ -27,28 +35,49 @@ const renderer = new WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const objects: Mesh[] = []
-for (let i = 0; i < 10; i++) {
-  const randomColor = Math.floor(Math.random() * 16777215);
-  const randomX = Math.floor((Math.random() - 0.5) * 1000)
-  const randomY = Math.floor((Math.random() - 0.5) * 1000)
-  const plane = new Mesh(
-    new PlaneGeometry(160, 72),
-    new MeshBasicMaterial({ color: randomColor })
+let instanceCount = 30_000;
+let mesh = new InstancedMesh(
+  new PlaneGeometry(1, 1),
+  new MeshBasicMaterial(),
+  instanceCount
+);
+
+const objects: Array<{
+  index: number;
+  color: Color;
+  matrix: Matrix4;
+}> = [];
+
+for (let i = 0; i < instanceCount; i++) {
+  const color = new Color(Math.floor(Math.random() * 16_777_215));
+  const matrix = new Matrix4();
+  matrix.compose(
+    new Vector3(
+      Math.floor((Math.random() - 0.5) * 10_000),
+      Math.floor((Math.random() - 0.5) * 10_000),
+      99.9999 + i / 1_000_000
+    ),
+    new Quaternion(),
+    new Vector3(160, 70, 1)
   );
-  plane.position.set(randomX, randomY, 99.9999 + i / 1000000);
-  objects.push(plane)
+  objects.push({ index: i, color, matrix });
+  mesh.setColorAt(i, color);
+  mesh.setMatrixAt(i, matrix);
 }
 
-objects.forEach(obj => scene.add(obj))
+scene.add(mesh);
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  stats.update()
-  objects.forEach(obj => {
-      obj.position.x += Math.random() < 0.5 ? -1 : 1
-      obj.position.y += Math.random() < 0.5 ? -1 : 1
-  })
+  stats.update();
+  objects.forEach((obj) => {
+    const vector = new Vector3().setFromMatrixPosition(obj.matrix);
+    vector.x += Math.random() < 0.5 ? -1 : 1;
+    vector.y += Math.random() < 0.5 ? -1 : 1;
+    obj.matrix.setPosition(vector);
+    mesh.setMatrixAt(obj.index, obj.matrix);
+  });
+  mesh.instanceMatrix.needsUpdate = true;
 }
 animate();
